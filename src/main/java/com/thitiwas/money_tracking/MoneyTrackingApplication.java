591edb34ffcba169991cae89b1387643108a0ed4
LAPTOP-9QEP4400\peegh <thitiwas.nn@gmail.com>
2023-05-21 15:28:23 +0700

    first

---
+++ src/main/java/com/thitiwas/money_tracking/MoneyTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.thitiwas.money_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTrackingApplication.class, args);
+	}
+
+}
